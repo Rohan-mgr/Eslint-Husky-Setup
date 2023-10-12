426ee888a40d0bc7eeb1d4b964679d203581bf11
Rohan-mgr <rohan.magar.415@gmail.com>
2023-10-12 13:47:17 +0545

    completed setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-console': 'error', // for preventing console.log to commit
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 
 function App() {
-  console.log('this is a log')
   return (
     <>
       <h1>Rohan Magar</h1>
